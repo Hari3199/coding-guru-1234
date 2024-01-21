2646646337e0aca95040181b03659ddc1ad10f4d
Hari3199 <nainaharesh31@gamil.com>
2024-01-21 16:40:58 +0530

    added

--- login.js
+++ login.js
@@ -1,33 +1,25 @@
 const form = document.querySelector("form");
-
 const email = document.getElementById("email");
 const password = document.getElementById("password");
 const submit_btn = document.getElementById("submit-btn");
 const url = "https://mock-api-template-j6kc.onrender.com/register";
 const baseurl = `https://mock-api-template-j6kc.onrender.com/`;
-submit_btn.addEventListener("submit", (e) => {
+form.addEventListener("submit", (e) => {
   e.preventDefault();
   handleSubmit();
 });
-async function handleSubmit(e) {
+async function handleSubmit(){
   try {
-    //preventing from form submitting
-
-    // let obj = {
-    //   email:email.value.trim(),
-    //   password:password.trim(),
-    // };
-
     let res = await fetch(`${url}`);
     let data = await res.json();
     console.log(data);
     let f = data.filter((ele) => {
-      if (ele.email == email.value && ele.password == password) {
+      if (ele.email == email.value.trim() && ele.password == password.value.trim()) {
         return ele;
       }
     });
     if (f.length > 0) {
-      window.location.href = "registaion2.html";
+      window.location.href = "registation2.html";
     } else {
       window.location.href = "signup.html";
     }
